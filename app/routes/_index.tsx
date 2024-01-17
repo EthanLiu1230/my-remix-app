aefb32c16c140fa7e8f1c6bb5ff5c14c65bfe68c
Ethan Liu <1230lyx@gmail.com>
2024-01-17 08:46:49 +0800

    Index Routes

---
+++ app/routes/_index.tsx
@@ -0,0 +1,14 @@
+/**
+ * The route name _index is special. It tells Remix to match and render this route when the user is at the parent route's exact path, so there are no other child routes to render in the <Outlet />.
+ */
+
+export default function Index() {
+  return (
+    <p id="index-page">
+      This is a demo for Remix.
+      <br />
+      Check out{" "}
+      <a href="https://remix.run">the docs at remix.run</a>.
+    </p>
+  );
+}
